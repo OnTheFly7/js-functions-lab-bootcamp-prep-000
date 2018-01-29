6d9a50d95cc0009d6d1c3ae78c6b1bf3346be6e2
Brian Senick <bsenick07@yahoo.com>
2018-01-29 01:13:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
+}
 
 
 
